52efbc0fa7f71bc1431bc8dedd1f23f23ee2c392
pepitoperezsha-cmyk <pepitoperezsha@gmail.com>
2026-03-23 18:53:52 -0700

    Gitignore file added

---
+++ blog/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Post, Comment, Category
+
+class CommentItemInliene(admin.TabularInline):
+    model = Comment
+    raw_id_fields = ['Post']
+
+class PostAdmin(admin.ModelAdmin):
+    search_fields = ['title','intro','body']
+    list_display =('title','category','created_at','status')
+    list_filter = ['category','created_at','status']
+    inlines = [CommentItemInliene]
+
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ['title']
+    list_display =('title')
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display =('name','post','created_at')
+
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(Post,PostAdmin)
+admin.site.register(Comment,CommentAdmin)
