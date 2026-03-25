9a153b85bd7ac8424daf26d9bb8ce1758223c892
pepitoperezsha-cmyk <pepitoperezsha@gmail.com>
2026-03-24 19:33:09 -0700

    nose

--- blog/admin.py
+++ blog/admin.py
@@ -3,23 +3,24 @@ from django.contrib import admin
 # Register your models here.
 from .models import Post, Comment, Category
 
-class CommentItemInliene(admin.TabularInline):
+class ComentItemInliene(admin.TabularInline):
     model = Comment
-    raw_id_fields = ['Post']
+    raw_id_fields = ['post']
 
 class PostAdmin(admin.ModelAdmin):
-    search_fields = ['title','intro','body']
-    list_display =('title','category','created_at','status')
-    list_filter = ['category','created_at','status']
-    inlines = [CommentItemInliene]
+    search_fields = ['title', 'intro', 'body']
+    list_display = ('title', 'category', 'created_at', 'status')
+    list_filter = ['category', 'created_at', 'status']
+    inlines = [ComentItemInliene]
 
 class CategoryAdmin(admin.ModelAdmin):
     search_fields = ['title']
-    list_display =('title', )
+    list_display = ('title',)
 
 class CommentAdmin(admin.ModelAdmin):
-    list_display =('name','post','created_at', )
+    list_display = ('name', 'post', 'created_ad', )    
 
-admin.site.register(Category,CategoryAdmin)
-admin.site.register(Post,PostAdmin)
-admin.site.register(Comment,CommentAdmin)
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
