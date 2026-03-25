4d461c67f3ac2d1bf5a3739469db6acccf56dba1
pepitoperezsha-cmyk <pepitoperezsha@gmail.com>
2026-03-24 19:11:10 -0700

    nose

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-
+#-*- coding: utf-8 -*-
 from django.db.models import Q
 from django.shortcuts import render, get_object_or_404, render 
 
@@ -11,3 +11,11 @@ def home(request):
         'posts': posts
     }
     return render(request, 'blog/home.html', context)
+
+def detail(request, id):
+    post = get_object_or_404(Post, id=id, status=Post.ACTIVATE)
+
+    context = {
+        'post': post,
+    }
+    return render(request, 'blog/detail.html', context)
