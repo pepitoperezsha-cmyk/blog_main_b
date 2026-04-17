3e0e203ec7f74fe04cda4346c9403c163585fffe
pepitoperezsha-cmyk <pepitoperezsha@gmail.com>
2026-04-17 16:07:28 -0700

    nose

--- blog/forms.py
+++ blog/forms.py
@@ -6,4 +6,20 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['name', 'email', 'body']
+        widgets = {
+        'name': forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Tu nombre'
+        }),
+        'email': forms.Textarea(attrs={
+            'class': 'form-control',
+            'rows': 1,
+            'placeholder': 'tu email'
+        }),
+        'body':forms.Textarea(attrs={
+            'class': 'form-control',
+            'rows': 5,
+            'placeholder': 'Escribe tu comentario'
+        }),
+        }
         
